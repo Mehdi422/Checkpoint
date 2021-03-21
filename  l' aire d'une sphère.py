f95ffd89a43cbc4347f22d9e3b95cab8c5c86b72
Mehdi422 <81095669+Mehdi422@users.noreply.github.com>
2021-03-21 16:35:30 +0100

    Add files via upload

---
+++  l' aire d'une sphère.py
@@ -0,0 +1,9 @@
+import math
+PI = math.pi
+if RAERY == input("Qu'est ce que tu veux: ")                                               
+   RAERY = "Je veut calculerl'aire de la sphère"
+else:
+print ('no')
+R = float(input("Saisissez Le rayon : "))
+Air = R * R * PI * 4
+print("L'air de la sphere est:",Air)
